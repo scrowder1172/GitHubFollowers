465ee566c657c86dc77c6adeaa4b12e47291ca76
Scott Crowder <scrowder@gmail.com>
2024-05-01 17:48:43 -0500

    Initial Commit

---
+++ GitHubFollowers/GitHubFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubFollowersApp.swift
+//  GitHubFollowers
+//
+//  Created by SCOTT CROWDER on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
