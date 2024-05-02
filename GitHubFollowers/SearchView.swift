3c84d964652834acdf5f8d1a4dc08cf347da51da
Scott Crowder <scrowder@gmail.com>
2024-05-02 15:00:17 -0500

    Create layout for Search View

--- GitHubFollowers/SearchView.swift
+++ GitHubFollowers/SearchView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State private var searchText: String = ""
+    
     var body: some View {
-        ContentUnavailableView(label: {
-            Image(.ghLogo)
-                .resizable()
-                .scaledToFit()
-        }, description: {
-            Text("This view will be replaced by the Search View")
-        })
+        NavigationStack{
+            VStack {
+                Image(.ghLogo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .padding(.bottom, 50 )
+                    .padding(.top, 80)
+                
+                GHFTextField(inputText: $searchText)
+                    .frame(height: 50)
+                    .padding(.horizontal, 50)
+                
+                Spacer()
+                
+                GHFButton(backgroundColor: .green, title: "Get Followers") {
+                    print("Button clicked")
+                }
+                .frame(height: 50)
+                .padding(.horizontal, 50)
+                .padding(.bottom, 50)
+            }
+            .background(Color.systemBackground)
+            .toolbar(.hidden) // hides navbar
+        }
     }
 }
 
 #Preview {
     SearchView()
 }
+
+#Preview("TabView") {
+    GHFTabView()
+}
+
