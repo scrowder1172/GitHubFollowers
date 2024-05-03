53a1218ec2320f12da29f352eb736719a265a903
Scott Crowder <scrowder@gmail.com>
2024-05-02 19:30:07 -0500

    add view extension for custom alert that disables and blurs the main view while showing the alert button

--- GitHubFollowers/View+Ext.swift
+++ GitHubFollowers/View+Ext.swift
@@ -5,4 +5,19 @@
 //  Created by SCOTT CROWDER on 5/2/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func ghfAlert(isShowingAlert: Binding<Bool>, title: String, message: String, buttonText: String) -> some View {
+        ZStack {
+            self
+                .disabled(isShowingAlert.wrappedValue)
+                .blur(radius: isShowingAlert.wrappedValue ? 3 : 0 )
+                .opacity(isShowingAlert.wrappedValue ? 0.3 : 1)
+            
+            if isShowingAlert.wrappedValue {
+                GHFAlertView(isShowingView: isShowingAlert, alertTitle: title, alertMessage: message, alertButtonText: buttonText)
+            }
+        }
+    }
+}
