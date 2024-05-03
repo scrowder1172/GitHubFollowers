55ec66523fdcb4bc50a3351aeb6f9e7ca5231ed8
Scott Crowder <scrowder@gmail.com>
2024-05-03 11:37:37 -0500

    create getFollowers function to retrieve array of Follower for given github user

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private let baseUrl: String = "https://api.github.com/users/"
+    
+    private init() {
+        
+    }
+    
+    func getFollowers(for username: String, page: Int, resultsPerPage: Int = 100) async throws -> [Follower] {
+        let endpoint: String = baseUrl + "\(username)/followers?per_page=\(resultsPerPage)&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            throw NetworkError.urlInvalid
+        }
+        
+        let (data, response): (Data, URLResponse)
+        
+        do {
+            (data, response) = try await URLSession.shared.data(from: url)
+        } catch {
+            throw NetworkError.responseInvalid
+        }
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw NetworkError.statusCodeInvalid
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let followers: [Follower] = try decoder.decode([Follower].self, from: data)
+            return followers
+            
+        } catch {
+            print("Error: \(error.localizedDescription)")
+            throw NetworkError.responseInvalid
+        }
+        
+    }
+}
