5c1d72248342f22e79718b1898321deddee95b35
Scott Crowder <scrowder@gmail.com>
2024-05-03 09:41:12 -0500

    create models for followers and users

---
+++ GitHubFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by SCOTT CROWDER on 5/3/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by SCOTT CROWDER on 5/3/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
