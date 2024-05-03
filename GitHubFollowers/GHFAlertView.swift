55d1ac584a48ca267d36b97fbe4e91df241866c2
Scott Crowder <scrowder@gmail.com>
2024-05-02 19:31:35 -0500

    create custom alert view

--- GitHubFollowers/GHFAlertView.swift
+++ GitHubFollowers/GHFAlertView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct GHFAlertView: View {
+    
+    @Binding var isShowingView: Bool
+    
+    let alertTitle: String
+    let alertMessage: String
+    let alertButtonText: String
+    
+    init(isShowingView: Binding<Bool>, alertTitle: String, alertMessage: String, alertButtonText: String = "OK") {
+        self._isShowingView = isShowingView
+        self.alertTitle = alertTitle
+        self.alertMessage = alertMessage
+        self.alertButtonText = alertButtonText
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            GHFTitleLabelView(titleText: alertTitle, textAlignment: .center, fontSize: 20)
+                .padding(.bottom, 20)
+            
+            GHFBodyLabelView(bodyText: alertMessage, textAlignment: .center)
+                .padding(.horizontal)
+                .padding(.bottom)
+            
+            GHFButton(backgroundColor: .pink, title: "OK") {
+                isShowingView = false
+            }
+            .padding(.horizontal)
+        }
+        .frame(width: 280, height: 200)
+        .background(Color.systemBackground)
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(.secondary, lineWidth: 2)
+        )
+        .animation(.easeOut, value: isShowingView)
     }
 }
 
 #Preview {
-    GHFAlertView()
+    GHFAlertView(
+        isShowingView: .constant(true),
+        alertTitle: "Empty Username",
+        alertMessage: "Please enter a username. We need to know who to look for 😄",
+        alertButtonText: "OK"
+    )
 }
