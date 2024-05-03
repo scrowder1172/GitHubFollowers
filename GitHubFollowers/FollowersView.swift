65153eb998391c518683e895c895927ec388b921
Scott Crowder <scrowder@gmail.com>
2024-05-03 11:38:16 -0500

    display followers when view loads. present error message if NetworkManager throws error

--- GitHubFollowers/FollowersView.swift
+++ GitHubFollowers/FollowersView.swift
@@ -11,14 +11,36 @@ struct FollowersView: View {
     
     let gitHubUser: String
     
+    @State private var isShowingAlert: Bool = false
+    @State private var errorMessage: String = "An error has occurred. Please try again."
+    @State private var followers: [Follower]?
+    
     var body: some View {
         VStack{
-            ContentUnavailableView(label: {
-                Label("Followers", systemImage: "person.3.fill")
-            }, description: {
-                Text("This will be a list of followers for \(gitHubUser)")
-            })
+            if let followers {
+                List(followers) { follower in
+                    Text(follower.login)
+                }
+                Text("Follower count: \(followers.count)")
+            } else {
+                ContentUnavailableView(label: {
+                    Label("Followers", systemImage: "person.3.fill")
+                }, description: {
+                    Text("This will be a list of followers for \(gitHubUser)")
+                })
+            }
         }
+        .onAppear {
+            Task {
+                do {
+                    followers = try await NetworkManager.shared.getFollowers(for: gitHubUser, page: 1)
+                } catch {
+                    errorMessage = error.localizedDescription
+                    isShowingAlert = true
+                }
+            }
+        }
+        .ghfAlert(isShowingAlert: $isShowingAlert, title: "Error!", message: errorMessage, buttonText: "OK")
         .navigationTitle(gitHubUser)
         .navigationBarTitleDisplayMode(.large)
     }
