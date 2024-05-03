6636f117d833ea35bf2cc92a86c036668963f1be
Scott Crowder <scrowder@gmail.com>
2024-05-03 11:36:16 -0500

    specify error handling messages

--- GitHubFollowers/Enums/NetworkError.swift
+++ GitHubFollowers/Enums/NetworkError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum NetworkError: Error, LocalizedError {
+    case urlInvalid
+    case statusCodeInvalid
+    case responseInvalid
+    case other(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .urlInvalid:
+            return "This username created an invalid request. Please try again."
+        case .statusCodeInvalid:
+            return "Invalid response from the server. Please try again."
+        case .responseInvalid:
+            return "Unable to complete your request. Please check your network connection."
+        case .other(let error):
+            return error.localizedDescription
+        }
+    }
+}
