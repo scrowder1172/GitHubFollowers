eaeca8b3b84e0682274037fd0d5f31932b7a45f5
Scott Crowder <scrowder@gmail.com>
2024-05-06 15:04:35 -0500

    create user detail view

---
+++ GitHubFollowers/UserDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailView.swift
+//  GitHubFollowers
+//
+//  Created by SCOTT CROWDER on 5/6/24.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailView()
+}
